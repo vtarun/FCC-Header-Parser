5696f7bda6fb2e37cc02e0fd3d6dcfc0148001c0
vtarun <vivektarun89@gmail.com>
2017-12-16 23:17:50 +0530

    some change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var api = require('./routes/route');
-var port = 3000;
+var port = process.env.PORT || 3000	;
 var app = express();
 
 app.use(express.static(__dirname + '/'));
