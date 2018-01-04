d279a94525df2ff01ddf2a691ed646cf56c3639c
vtarun <vivektarun89@gmail.com>
2018-01-04 13:24:23 +0530

    modified

--- routes/route.js
+++ routes/route.js
@@ -3,9 +3,12 @@ var express = require('express');
 var route = express.Router();
 
 route.get('/header', function(req, res){
-	var software = req.headers['user-agent'];
-	//res.send(req.headers);
-	res.json({'ip' : req.connection.remoteAddress, 'language' : (req.headers['accept-language']).split(',')[0], "software": software.substring(software.indexOf('(') + 1,software.indexOf(')')) });	
+	var software = req.headers['user-agent'];	
+	var ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+	res.json({'ip' : ip, 'language' : (req.headers['accept-language']).split(',')[0], "software": software.substring(software.indexOf('(') + 1,software.indexOf(')')) });	
 });
 
 module.exports = route;
